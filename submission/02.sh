e2f409db339df54d086e7de2e3c4de8e2700940e
katocollins <ckato131@gmail.com>
2026-03-18 22:30:56 +0300

    Submission of week 1 scripts

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bitcoin-cli -regtest getblockchaininfo | grep -o '"chain":"[^"]*"' | cut -d'"' -f4
+bitcoin-cli -regtest getblockchaininfo | jq -r '.chain'
--- submission/10.sh
+++ submission/10.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | grep -o '"isvalid":[^,}]*' | cut -d':' -f2 | tr -d ' '
-
+bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | jq -r '.isvalid'
